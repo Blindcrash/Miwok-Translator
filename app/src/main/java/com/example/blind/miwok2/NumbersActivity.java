83d537806bc2335d5a603e163126633e06c58a87
Blindcrash <blindcrash65@gmail.com>
2019-01-22 00:20:08 -0500

    Added Fragment for the last updated comming

--- app/src/main/java/com/example/blind/miwok2/NumbersActivity.java
+++ app/src/main/java/com/example/blind/miwok2/NumbersActivity.java
@@ -3,8 +3,8 @@ package com.example.blind.miwok2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class NumbersActivity extends AppCompatActivity {
 
+public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
